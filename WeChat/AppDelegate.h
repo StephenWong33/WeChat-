b406e1b345467f57f39fd41e2b9eb52b58beaaf7
StephenWong33 <1030751708@qq.com>
2016-01-14 14:03:06 +0800

    Initial Commit

---
+++ WeChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeChat
+//
+//  Created by 王亚帅 on 16/1/14.
+//  Copyright © 2016年 stephen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WeChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeChat
+//
+//  Created by 王亚帅 on 16/1/14.
+//  Copyright © 2016年 stephen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
